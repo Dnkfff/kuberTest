2cb6958b980d16aedde22c50b3f3fe30d762ceb4
Dnkfff <ddynikov@gmail.com>
2025-04-08 15:28:18 +0300

    initial

---
+++ index.js
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// // import { createRoot } from 'react-dom/client';
+
+// import './App.css';
+// import HomePage from './components/HomePage';
+
+// function App() {
+//   return (
+//     <Router basename="http://localhost:3000/">
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         {/* Add more routes as needed */}
+//       </Routes>
+//     </Router>
+//   );
+// }
+// export default App;
